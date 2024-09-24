0a83734fb20fca6b57b99e3c7696266e390168a0
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-24 14:57:00 +0200

    Initial commit

---
+++ src/main/java/com/Jari/API_List/ApiListApplication.java
@@ -0,0 +1,13 @@
+package com.Jari.API_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jari/API_List/ApiListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jari.API_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
